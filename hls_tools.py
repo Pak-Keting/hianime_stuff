d5ad2cd0662a3b247edc6722b6a17c7df8d40f19
Pak-Keting <paketipang88@gmail.com>
2025-11-14 09:42:41 +0800

    add simple ts file integrity checker

--- hls_tools.py
+++ hls_tools.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 def localize_m3u8(m3u8: str, video_path: str) -> str:
-    lines = []
+    lines: list = []
     for i in m3u8.split():
         if "https://" in i:
-            filename_fixed = Path(i).stem + ".ts"
+            filename_fixed: str = Path(i).stem + ".ts"
             lines.append(str(Path(video_path) / filename_fixed))
         else:
             lines.append(i)
@@ -17,6 +17,26 @@ def get_segment_links(m3u8: str) -> list:
 
 def get_segment_filenames_fixed_extension(m3u8: str) -> list:
     return [Path(i).stem + ".ts" for i in m3u8.split() if "https://" in i]
+
+
+def is_valid_ts(filename: str) -> bool:
+    file_size: int = Path(filename).stat().st_size
+    if file_size < 1000: # a segment below 1kb is probably corrupted
+        return False;
+
+    with open(filename,'rb') as f:
+        data = f.read(1024)
+
+    # checking mpegts sync byte at beginning
+    if data[0] != 0x47:
+        return False
+
+    # checking the subsequent mpegts sync byte, it presented every 188 bytes
+    for offset in (188,188*2,188*3,188*4,188*5):
+        if offset < len(data) and data[offset] != 0x47:
+            return False
+
+    return True
     
 
 
@@ -27,4 +47,7 @@ if __name__ == "__main__":
     a = localize_m3u8(m3u8,'/dsg/ageha/hbwh/jsxdf/')
     b = get_segment_links(m3u8)
     c = get_segment_filenames_fixed_extension(m3u8)
-    print(a)
+    #print(c)
+
+    print(is_valid_ts("./samples/seg-1-v1-a1.jpg"))
+    print(is_valid_ts("./samples/index-f3-v1-a1.m3u8"))
