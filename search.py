a30a42e05d704ed541306663b25be17f2152992e
Pak-Keting <paketipang88@gmail.com>
2026-01-17 22:54:51 +0800

    search.py: remove redundancy of the query string logic

--- search.py
+++ search.py
@@ -9,12 +9,14 @@ import hianime_tools
 if(len(sys.argv) != 2):
     print(sys.argv[0] + " requires 1 argument!\nexiting.")
     exit(1)
-search_query: str = sys.argv[1].replace(' ', '+')
 
-
-html: str = r.get(hianime_tools.BASE_LINK + "/ajax/search/suggest?keyword=" + search_query).json().get("html")
+html: str = r.get(
+    hianime_tools.BASE_LINK + "/ajax/search/suggest",
+    params={"keyword": sys.argv[1]}
+).json().get("html")
 search_results = hianime_tools.parse_search_result(html)
 max_len = max(len(title) for title in search_results)
 
 for title, link in search_results.items():
     print(f"{title:<{max_len + 2}}{hianime_tools.BASE_LINK}/watch{link}")
+print()
